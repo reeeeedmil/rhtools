4e9f832a4cc30102d96575b6a7fc26bebcb105b1
reeeeedmil <agrlul@email.cz>
2024-03-26 09:06:49 +0100

    small changes + cargo fmt

--- src/convert.rs
+++ src/convert.rs
@@ -1,12 +1,27 @@
-pub fn show_binary(input: Option<u32>) {
+use crate::ConvertGroup;
+
+pub fn convert(input: ConvertGroup) {
+    if input.binary != None {
+        show_binary(input.binary);
+    } else if input.hexadecimal != None {
+        show_hexadecimal(input.hexadecimal);
+    } else {
+        show_octal(input.octal);
+    }
+}
+
+fn show_binary(input: Option<u32>) {
     let input = match input {
         Some(num) => num,
-        None => {println!("number is none"); std::process::exit(0);}
+        None => {
+            println!("number is none");
+            std::process::exit(0);
+        }
     };
     let bin = String::from(format!("{:b}", input));
     let mut empty_string: String = String::new();
     for n in 0..bin.len() {
-        if n%4 == 0 && n != 0 {
+        if n % 4 == 0 && n != 0 {
             empty_string.push(char::from(32));
         }
         empty_string.push(bin.chars().nth(n).unwrap());
@@ -14,30 +29,36 @@ pub fn show_binary(input: Option<u32>) {
     println!("Input: {}\nBin: {}", input, empty_string);
 }
 
-pub fn show_hexadecimal(input: Option<u32>) {
+fn show_hexadecimal(input: Option<u32>) {
     let input = match input {
         Some(num) => num,
-        None => {println!("number is none"); std::process::exit(0);}
+        None => {
+            println!("number is none");
+            std::process::exit(0);
+        }
     };
     let bin = String::from(format!("{:x}", input));
     let mut empty_string: String = String::new();
     for n in 0..bin.len() {
-        if n%2 == 0 && n != 0 {
+        if n % 2 == 0 && n != 0 {
             empty_string.push(char::from(32));
         }
         empty_string.push(bin.chars().nth(n).unwrap());
     }
     println!("Input: {}\nHex: {}", input, empty_string);
 }
-pub fn show_octal(input: Option<u32>) {
+fn show_octal(input: Option<u32>) {
     let input = match input {
         Some(num) => num,
-        None => {println!("number is none"); std::process::exit(0);}
+        None => {
+            println!("number is none");
+            std::process::exit(0);
+        }
     };
     let bin = String::from(format!("{:o}", input));
     let mut empty_string: String = String::new();
     for n in 0..bin.len() {
-        if n%3 == 0 && n != 0 {
+        if n % 3 == 0 && n != 0 {
             empty_string.push(char::from(32));
         }
         empty_string.push(bin.chars().nth(n).unwrap());
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-mod copy;
 mod convert;
-
+mod copy;
 
 use clap::{Parser, Subcommand};
-use convert::{show_binary, show_hexadecimal, show_octal};
+use convert::convert;
 
 use crate::copy::copy;
 
 const VERSION: &str = "0.1.0";
-const ABOUT: &str = "Tools that I needed standardized because I hate jumping between linux and windows.";
+const ABOUT: &str =
+    "Tools that I needed standardized because I hate jumping between linux and windows.";
 const AUTHOR: &str = "reeeeedmil";
 #[derive(Debug, clap::Args, Clone)]
 #[group(required = true, multiple = false)]
 pub struct ConvertGroup {
     /// Convert number to binary
-    #[clap(short='b', long, value_name="NUMBER")]
+    #[clap(short = 'b', long, value_name = "NUMBER")]
     binary: Option<u32>,
     /// Convert number to hexadecimal
-    #[clap(short='x', long, value_name="NUMBER")]
+    #[clap(short = 'x', long, value_name = "NUMBER")]
     hexadecimal: Option<u32>,
     /// Convert number to octal
-    #[clap(short='o', long, value_name="NUMBER")]
+    #[clap(short = 'o', long, value_name = "NUMBER")]
     octal: Option<u32>,
 }
 
@@ -32,34 +32,27 @@ struct Args {
 }
 #[derive(Subcommand, Debug, Clone)]
 enum Commands {
+    /// Copies directory and everything inside it to a selected destination
     Copy {
         #[arg(short, long)]
         source_path: std::path::PathBuf,
         #[arg(short, long)]
         destination_path: std::path::PathBuf,
-
     },
+    /// Converts decimal number to binary/hexadecimal/octal
     Convert {
         #[clap(flatten)]
         input: ConvertGroup,
-    }
+    },
 }
 
 fn main() {
     let args = Args::parse();
     match args.cmd {
-        Commands::Copy{source_path, destination_path} => copy(source_path, destination_path),
-        Commands::Convert { input } => {
-            if input.binary != None {
-                show_binary(input.binary);
-            }
-            else if input.hexadecimal != None {
-                show_hexadecimal(input.hexadecimal);
-            }
-            else {
-                show_octal(input.octal);
-            }
-        },
-
+        Commands::Copy {
+            source_path,
+            destination_path,
+        } => copy(source_path, destination_path),
+        Commands::Convert { input } => convert(input),
     }
 }
